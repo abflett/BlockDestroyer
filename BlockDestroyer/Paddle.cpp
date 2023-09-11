74d4be08af8f839a8db62c1c1641b34b8833b8e9
Adam Flett <Adam@adamflett.net>
2023-09-11 22:18:29 +0800

    changed size

--- BlockDestroyer/Paddle.cpp
+++ BlockDestroyer/Paddle.cpp
@@ -3,8 +3,8 @@
 #include <SDL.h>
 
 namespace BlockDestroyer {
-	const int Paddle::width = 96;
-	const int Paddle::height = 16;
+	const int Paddle::width = 72;
+	const int Paddle::height = 8;
 	const int Paddle::y = PLAYFIELD_HEIGHT - 40;
 
 	Paddle::Paddle(int x) : x(x), leftIsPressed(false), rightIsPressed(false) {}
@@ -33,7 +33,7 @@ namespace BlockDestroyer {
 		float speed = 1;
 
 		if (leftIsPressed && x > 0) {
-			x += static_cast<int>(-1 * deltaTime);
+			x += static_cast<int>(-1 * (deltaTime * 0.5));
 		}
 
 		if (x < 0) {
@@ -41,7 +41,7 @@ namespace BlockDestroyer {
 		}
 
 		if (rightIsPressed && x < PLAYFIELD_WIDTH - width) {
-			x += static_cast<int>(1 * deltaTime);
+			x += static_cast<int>(1 * (deltaTime * 0.5));
 		}
 
 		if (x > PLAYFIELD_WIDTH - width) {
